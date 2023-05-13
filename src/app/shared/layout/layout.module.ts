353c0520cb5a2c1f8d284f4c97bc9258e8c58d2e
olahaye <olahaye@actiris.be>
2023-05-13 22:13:19 +0200

    Create layout-header + impl.

--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -7,11 +7,12 @@ import { ConfirmService } from './confirm.service';
 import { EditionLayoutComponent } from './edition-layout/edition-layout.component';
 import { ListingLayoutComponent } from './listing-layout/listing-layout.component';
 import { RemoveButtonComponent } from './remove-button/remove-button.component';
+import { LayoutHeaderComponent } from './layout-header/layout-header.component';
 
 @NgModule({
-    declarations: [RemoveButtonComponent, AddButtonComponent, ListingLayoutComponent, EditionLayoutComponent, ConfirmModalComponent],
+    declarations: [RemoveButtonComponent, AddButtonComponent, ListingLayoutComponent, EditionLayoutComponent, ConfirmModalComponent, LayoutHeaderComponent],
     imports: [CommonModule, MaterialModule],
-    exports: [RemoveButtonComponent, AddButtonComponent, ListingLayoutComponent, EditionLayoutComponent, ConfirmModalComponent],
+    exports: [RemoveButtonComponent, AddButtonComponent, ListingLayoutComponent, EditionLayoutComponent, ConfirmModalComponent, LayoutHeaderComponent],
     providers: [ConfirmService],
 })
 export class LayoutModule {}
