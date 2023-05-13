48a32a228eb4abcf344f0845345421e184e0d36b
olahaye <olahaye@actiris.be>
2023-05-13 15:10:43 +0200

    Update category service

--- src/app/core/category.service.ts
+++ src/app/core/category.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { of, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class CategoryService {
+    private _data: string[] = ['professional', 'private'];
 
-  constructor() { }
+    constructor() {}
+
+    /**
+     *
+     * @returns
+     */
+    getCategories() {
+        return of(this._data);
+    }
+
+    /**
+     *
+     * @param index
+     * @returns
+     */
+    getCategory(index: number) {
+        return index >= this._data.length || index < 0 ? throwError(() => new Error('Category not found')) : of(this._data[index]);
+    }
+
+    /**
+     * @param category
+     * @returns last inserted index
+     */
+    create(category: string) {
+        return of(this._data.push(category) - 1);
+    }
+
+    update(index: number, category: string) {
+        this._data[index] = category;
+        return of(null);
+    }
+
+    remove(index: number) {
+        this._data.splice(index, 1);
+        return of(null);
+    }
 }
