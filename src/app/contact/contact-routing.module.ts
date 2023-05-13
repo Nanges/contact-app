4278d1884d2bd47d00d80d7e12d45db2e41905b7
olahaye <olahaye@actiris.be>
2023-05-13 23:20:21 +0200

    Setup contact form

--- src/app/contact/contact-routing.module.ts
+++ src/app/contact/contact-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactResolver } from '../core/resolvers/contact.resolver';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
 
 const routes: Routes = [
     {
         path: '',
         component: ContactListComponent,
+    },
+    {
+        path: 'add',
+        component: ContactFormComponent,
+    },
+    {
+        path: 'edit/:contactId',
+        resolve: {
+            contact: ContactResolver,
+        },
+        component: ContactFormComponent,
     },
 ];
 
--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -5,9 +5,10 @@ import { LayoutModule } from '../shared/layout/layout.module';
 import { MaterialModule } from '../shared/material/material.module';
 import { ContactListComponent } from './contact-list/contact-list.component';
 import { ContactRoutingModule } from './contact-routing.module';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
-    declarations: [ContactListComponent],
+    declarations: [ContactListComponent, ContactFormComponent],
     imports: [CommonModule, ContactRoutingModule, LayoutModule, MaterialModule, ReactiveFormsModule],
 })
 export class ContactModule {}
