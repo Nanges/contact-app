0dfa118b814a187b8ffd80753839544d3cb8d9f2
olahaye <olahaye@actiris.be>
2023-05-13 13:04:19 +0200

    Setup modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'categories',
+        loadChildren: () => import('./category/category.module').then((m) => m.CategoryModule),
+    },
+    {
+        path: 'contacts',
+        loadChildren: () => import('./contact/contact.module').then((m) => m.ContactModule),
+    },
+    {
+        path: '**',
+        redirectTo: 'contacts',
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
