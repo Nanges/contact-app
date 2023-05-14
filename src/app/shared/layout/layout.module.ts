4003e2964eb3a0069cdbee6ada1a31d1098e25be
olahaye <olahaye@actiris.be>
2023-05-14 17:27:46 +0200

    Reorganise imports

--- src/app/shared/layout/layout.module.ts
+++ src/app/shared/layout/layout.module.ts
@@ -5,14 +5,28 @@ import { AddButtonComponent } from './add-button/add-button.component';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { ConfirmService } from './confirm.service';
 import { EditionLayoutComponent } from './edition-layout/edition-layout.component';
+import { LayoutHeaderComponent } from './layout-header/layout-header.component';
 import { ListingLayoutComponent } from './listing-layout/listing-layout.component';
 import { RemoveButtonComponent } from './remove-button/remove-button.component';
-import { LayoutHeaderComponent } from './layout-header/layout-header.component';
 
 @NgModule({
-    declarations: [RemoveButtonComponent, AddButtonComponent, ListingLayoutComponent, EditionLayoutComponent, ConfirmModalComponent, LayoutHeaderComponent],
+    declarations: [
+        RemoveButtonComponent,
+        AddButtonComponent,
+        ListingLayoutComponent,
+        EditionLayoutComponent,
+        ConfirmModalComponent,
+        LayoutHeaderComponent,
+    ],
     imports: [CommonModule, MaterialModule],
-    exports: [RemoveButtonComponent, AddButtonComponent, ListingLayoutComponent, EditionLayoutComponent, ConfirmModalComponent, LayoutHeaderComponent],
+    exports: [
+        RemoveButtonComponent,
+        AddButtonComponent,
+        ListingLayoutComponent,
+        EditionLayoutComponent,
+        ConfirmModalComponent,
+        LayoutHeaderComponent,
+    ],
     providers: [ConfirmService],
 })
 export class LayoutModule {}
