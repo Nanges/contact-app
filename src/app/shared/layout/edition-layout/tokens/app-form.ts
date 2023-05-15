7d674c495d8e3e983f1eb660b0de5c7ea439789e
olahaye <olahaye@actiris.be>
2023-05-15 13:10:28 +0200

    Update token

--- src/app/shared/layout/edition-layout/tokens/app-form.ts
+++ src/app/shared/layout/edition-layout/tokens/app-form.ts
@@ -1,7 +1,7 @@
 import { InjectionToken, Provider } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
-export const APP_FORM = new InjectionToken<FormGroup>('Form used by edition layout');
+export const APP_FORM = new InjectionToken<FormGroup>('Form used by/in edition layout');
 
 export const appForm: (factory: () => FormGroup) => Provider = (factory: () => FormGroup) => ({
     provide: APP_FORM,
